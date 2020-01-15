136ec6aee09c07f16d760baca7e270e274b13722
isaeva anna <pangolin-666@mail.ru>
2020-01-15 18:19:58 +0300

    turn of debug

--- fulkek/settings.py
+++ fulkek/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')6#)=a^m@!*&8umj%63)kk%pyaq6_^$=sor0^evdd*!09$6vk('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
